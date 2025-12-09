0245da89cb65a5c6d882f55655053e76b10b1ec4
SHUBHJITENDRABHAIPRAJAPATI <shubhjitendrabhai.prajapati@student.sl.on.ca>
2025-12-09 16:03:19 -0500

    login and regirested routes added

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+// backend/models/user.js
+//Shubh Prajapati
+//Model for User schema
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema, "users");
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,24 @@ const server = express();
 const port = 3000;
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Product = require("./models/product");
+const Product = require("./models/product");//getting Product model
+const User = require("./models/user");//getting user model
+const bcrypt = require("bcrypt");//for hashing password
+const jwt = require("jsonwebtoken");//
 require("dotenv").config();
-const { DB_URI } = process.env;
+const { DB_URI, JWT_SECRET } = process.env;
 
+
+
+//setting up middleware
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
+
+
+//connecting to database and starting server
+//connect to mongoDB
 mongoose
   .connect(DB_URI)
   .then(() => {
@@ -33,6 +43,59 @@ server.get("/products", async (request, response) => {
     console.log(error.message);
   }
 });
+
+/* here,all the routes and CRUD will be written  */
+
+      /* $authentication routes */
+//register route
+server.post("/register", async (request, response) => {
+  const { username, password } = request.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newuser = new User({
+      username,
+      password: hashedPassword,
+    });
+    await newuser.save();
+    response.send({ message: "Great,User registered successfully" });
+  } catch (error) {
+    response
+      .status(500)
+      .send({ message: "user name already exists,please try again" });
+  }
+});
+
+
+
+//login route
+server.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return response.status(404).send({ message: "User is not found in the database" });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return response
+        .status(403)
+        .send({ message: "please check username or password" });
+    }
+
+
+    const jwtToken = jwt.sign({ id: user._id, username }, SECRET_KEY);
+    return response
+      .status(201)
+      .send({ message: "User Authenticated", token: jwtToken });
+  } catch (error) {
+    response.status(500).send({ message: error.message });
+  }
+});
+//login route ends here
+
+
 
 server.post("/add-product", async (request, response) => {
   const { productName, brand, image, price } = request.body;
