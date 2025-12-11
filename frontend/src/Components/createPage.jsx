9ee407302dc235df12f999169a5f8da1ecfa6e88
Jaden-Lloyd <jaden_lloyd@outlook.com>
2025-12-11 15:28:00 -0500

    My code for some frontend compoments

---
+++ frontend/src/Components/createPage.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function CreatePage() {
+  //states
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [postResponse, setPostResponse] = useState("");
+
+  //handlers
+  const handleOnChange = (e) => {
+    setFormData((prevData) => {
+      return { ...prevData, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/create-user", {
+        ...formData,
+      });
+      setPostResponse(response.data.message);
+    } catch (error) {
+      setPostResponse(error.response.data.message || "Cannot add username");
+    }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    handleRegister();
+    setFormData({ username: "", password: "" });
+  };
+}
