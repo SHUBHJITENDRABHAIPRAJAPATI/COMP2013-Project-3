29491cc4d8e28de66c41d8af74b7ce9e9f1bf060
jacobtummon33 <jacobtummon333@gmail.com>
2025-12-11 17:02:29 -0500

    Added routing for Jaden's work in advance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GroceriesAppContainer from "./Components/GroceriesAppContainer";
+import NotAuthorized from "./Components/NotAuthorized";
+import PageNotFound from "./Components/PageNotFound";
+
+//Jacob Tummon
 
 function App() {
   return (
@@ -8,6 +12,12 @@ function App() {
       <Routes>
         {/* TEMPORARY FOR DEBUGGING, GROCERIESAPPCONTAINER WILL BE "/main" */}
         <Route path="/" element={<GroceriesAppContainer />} />
+
+        {/*Not authorized page*/}
+        <Route path="/not-authorized" element={<NotAuthorized />} />
+
+        {/*Not found page, catches any paths that don't exist - KEEP AT BOTTOM OF ROUTES*/}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
